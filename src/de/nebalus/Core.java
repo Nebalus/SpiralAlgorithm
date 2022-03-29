e0773e3a1c5389ff1d442240587cd0d55a233e95
Aiden N <56486387+Nebalus@users.noreply.github.com>
2022-03-29 12:54:33 +0200

    Add files via upload

--- src/de/nebalus/Core.java
+++ src/de/nebalus/Core.java
@@ -3,8 +3,8 @@ package de.nebalus;
 public class Core {
 
 	public static void main(String[] args) {
-		SpiralAlgorithm sa = new SpiralAlgorithm(42);
-		System.out.println("X: "+sa.getXCord()+ " Y: "+sa.getYCord());
+		SpiralAlgorithm sa = new SpiralAlgorithm(-3, -4);
+		System.out.println("X: "+sa.getXCord()+ " Y: "+sa.getYCord()+" ID: "+sa.getUnit());
 	}
 	
 }
